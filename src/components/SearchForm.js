644917abbb4e452b364a9c98151e83a5c3600972
Katrina Finney <katrinafinney@gmail.com>
2019-11-15 15:53:21 -0500

    added component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from 'styled-components';
 
 export default function Characters() {
   const [data, setData] = useState([]);
@@ -20,6 +21,26 @@ export default function Characters() {
   const handleInputChange = event => {
     setQuery(event.target.value);
   };
+  const CharacterList = styled.div`
+  font-family: "Source Sans Pro", sans-serif;
+  background: #fff;
+  text-align: center;
+  color: #12cbe4;
+  padding: 10px;
+  padding-top:30px;
+  border-radius: 4px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: auto;
+  grid-gap: 20px;
+  grid-auto-flow: row;
+  justify-content: center;
+  align-items: start;
+  margin: 0 2rem;
+  margin-bottom: 2rem;
+`;
+
   return (
     <div className="spells">
       <form className="search" >
@@ -37,18 +58,15 @@ export default function Characters() {
       <div className="spell">
         {data.map(data => {
           return (
-            <div className="character-list " key={data._id}>
+            <CharacterList className="character-list " key={data._id}>
               <h2>
-                <span aria-label="wheel of dharma" role="img">
-                
-                </span>
                 {data.name}
               </h2>
               <h3 className="capital">Status: {data.status}</h3>
               <h3 className="capital">Species: {data.species}</h3>
               <h3 className="capital">Gender: {data.gender}</h3>
               
-            </div>
+            </CharacterList>
           );
         })}
       </div>
